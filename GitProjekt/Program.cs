84b6c7f78be1c934b209b19e9310a69f3a95bbdd
Adrian <Adrian@LAPTOP-IIUB3F29>
2017-09-12 10:53:51 +0200

    ändrat bokstav

--- GitProjekt/Program.cs
+++ GitProjekt/Program.cs
@@ -6,7 +6,7 @@ namespace GitProjekt
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Syntax rockar");
         }
     }
 }
